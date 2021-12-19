dcf7ad031b3fb24f26704b3d944d68190ae0c2ff
Salome <95755981+Salomeleshka@users.noreply.github.com>
2021-12-19 18:18:57 +0100

    Update js

--- script.js
+++ script.js
@@ -107,3 +107,5 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
+
+// test commit
